cb58bcdcb2228ea77896fee677d07d4b92187d19
austin li <645267294@qq.com>
2024-12-16 14:27:29 -0500

    fix some bug

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
-import testJWTRouter from "./controllers/test-jwt.js";
 import usersRouter from "./controllers/users.js";
 import profilesRouter from "./controllers/profiles.js";
 import hootsRouter from "./controllers/hoots.js";
@@ -17,8 +16,7 @@ mongoose.connection.on("connected", () => {
 app.use(cors());
 app.use(express.json());
 
-// Routes go here
-app.use("/test-jwt", testJWTRouter);
+
 app.use("/users", usersRouter);
 app.use("/profiles", profilesRouter);
 app.use("/hoots", hootsRouter);
