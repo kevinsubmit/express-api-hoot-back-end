9e8b98a869af5f53f141a1f4d40b9313d7e189ff
austin li <645267294@qq.com>
2024-12-16 20:30:30 -0500

    fix the id bug

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -90,7 +90,7 @@ router.delete("/:hootId", verifyToken, async (req, res) => {
       return res.status(403).send("You're not allowed to do that!");
     }
 
-    const deletedHoot = await Hoot.findByIdAndDelete(req.params.id);
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
     res.status(200).json(deletedHoot);
   } catch (error) {
     res.status(500).json(error);
